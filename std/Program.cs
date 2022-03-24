d287441adcf12df172976ea839179f1e49a5d781
apazala <albertopascual@hotmail.es>
2022-03-24 20:10:12 +0100

    add all

--- std/Program.cs
+++ std/Program.cs
@@ -10,6 +10,28 @@ namespace std
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            string line = Console.ReadLine();
+
+            int n = int.Parse(line);
+
+            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
+
+            int sum = arr.Sum();
+            double mean = sum / (double)n;
+
+            double sqDevSum = 0;
+            foreach(int val in arr)
+            {
+                double deviation = val - mean;
+                sqDevSum += deviation * deviation; 
+            }
+
+            double std = Math.Sqrt(sqDevSum/n);
+
+            Console.WriteLine($"{std:0.0}");
+
+            return;
         }
     }
 }
